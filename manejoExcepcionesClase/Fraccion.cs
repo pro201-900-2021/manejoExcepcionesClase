66e417287c3c4c93c27cf2fa114909e9d9273b9f
dotCero <lucero.inginfo@gmail.com>
2021-11-02 21:36:22 -0300

    Agregar archivos de proyecto.

---
+++ manejoExcepcionesClase/Fraccion.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace manejoExcepcionesClase
+{
+    class Fraccion
+    {
+        int numerador;
+        int denominador;
+
+        public Fraccion(int numerador, int denominador)
+        {
+            this.numerador = numerador;
+            this.denominador = denominador;
+        }
+
+        public int GetNumerador()
+        {
+            return numerador;
+        }
+
+        public void SetNumerador(int value)
+        {
+            numerador = value;
+        }
+
+        public int GetDenominador()
+        {
+            return denominador;
+        }
+
+        public void SetDenominador(int value)
+        {
+            denominador = value;
+        }
+
+        public double GetResultado()
+        {
+            double resultado;//solamente declaré la variable
+            try
+            {
+                resultado = numerador / denominador; //Excepción
+
+            }catch(DivideByZeroException ex)
+            {
+                Console.WriteLine("La división por cero no está definida.");
+                Console.WriteLine("Mensaje de error: "+ex.Message);
+                resultado = 0;
+            }catch(Exception ex)
+            {
+                Console.WriteLine("Ha ocurrido un error, contacte al área correspondiente(AA-00)");
+                resultado = 0;
+            }
+
+            return resultado;
+        }
+
+        public string GetVista()
+        {
+            //{N(entero)}
+            string vista = String.Format("{0}/{1}", numerador, denominador);
+            return vista;
+        }
+    }
+}
---
+++ manejoExcepcionesClase/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace manejoExcepcionesClase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creando Fracción");
+            Fraccion nuevaFraccion = new Fraccion(10, 5); // 10/5
+
+            Console.WriteLine("Resultado Fracción {0}", nuevaFraccion.GetResultado());
+            Console.WriteLine($"Resultado Fracción {nuevaFraccion.GetResultado()}");
+            Console.WriteLine("Vista de fracción: {0}", nuevaFraccion.GetVista());
+
+            Fraccion otraFraccion = new Fraccion(20, 0);
+            Console.WriteLine("Resultado Fracción {0}", otraFraccion.GetResultado());
+
+
+
+            //Lanzar Nueva Excepción, por lanzarla, por que necesito lanzar una excepción
+            throw new DivideByZeroException();
+        }
+    }
+}
